594a6649368f3330cc03ba87f4f3decbf3c16d5d
pablisEsp <pabelslopez@gmail.com>
2025-01-28 12:23:42 +0100

    added andfixed databases schemas

--- models/room_model.js
+++ models/room_model.js
@@ -1,24 +1,28 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const HabitacionSchema = new mongoose.Schema({
-    nombre: { type: String, required: true },
-    categoria: { type: String, required: true },
-    camas: [{
+  codigo: { type: String, required: true, unique: true }, // Usamos String para permitir códigos alfanuméricos
+  nombre: { type: String, required: true },
+  categoria: { type: String, required: true },
+  camas: [
+    {
       numCamas: { type: Number, required: true },
       nombre: { type: String, required: true },
-     }], 
-    tamanyo: { type: Number, required: true },
-    planta: { type: Number, required: true },
-    accesorios: [{ 
-      logo: { type: String }, 
-      descripcion: { type: String, required: true },
-    }],
-    numPersonas: { type: Number, required: true },
-    precio: { type: Number, required: true },
-    descripcion: { type: String },
-    imagenes: [{ type: String }] ,
-    habilitada: {type: Boolean, required : true}
-  });
-  
-  module.exports = mongoose.model('Habitacion', HabitacionSchema);
-  
+    },
+  ],
+  tamanyo: { type: Number, required: true }, // Área en m2
+  servicios: [{ type: String }], // Lista de servicios
+  numPersonas: { type: Number, required: true }, // Número de personas permitidas
+  precio: { type: Number, required: true }, // Precio por noche
+  descripcion: { type: String }, // Descripción opcional
+  imagenes: [
+    {
+      android: { type: String }, // URL de la imagen en Android
+      wpf: { type: String }, // URL de la imagen en WPF
+    },
+  ],
+  habilitada: { type: Boolean, required: true, default: true }, // Estado de disponibilidad
+});
+
+// Exportar el modelo
+module.exports = mongoose.model("Habitacion", HabitacionSchema);
